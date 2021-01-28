cfc0715a343426b7f6b044aa38fc1c3b7680af80
Ruslan Korolev <great1672@gmail.com>
2021-01-28 20:24:00 +0200

    Ability to add and index todos (#1)
    * Markup + CSS
    
    * Add ability to add todo and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import { AddTodo } from './features/todos/AddTodo'
+import { TodoList } from './features/todos/TodoList'
+
+const initialTodos: Todo[] = [
+  {
+    title: 'Ride my bicycle',
+    checked: false
+  },
+  {
+    title: 'Code to do list',
+    checked: true
+  },
+  {
+    title: 'Practice TypeScript',
+    checked: false
+  },
+  {
+    title: 'Take a breakfast',
+    checked: false
+  }
+]
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos)
+  const addTodo = (title: string) => {
+    const newTodos = [...todos, { title, checked: false }]
+    setTodos(newTodos)
+  }
+
+  const toggleTodo = (todo: Todo) => {
+    const newTodos = todos.map(el => {
+      if (el.title === todo.title) { // TODO: add id
+        return { ...el, checked: !el.checked }
+      }
+      return el
+    })
+        
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <AddTodo addTodo={addTodo}/>
+        <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      </div>
     </div>
   );
 }
